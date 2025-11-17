915dbaf347e12d0b81293807fec8f36d89c00807
Caitlin Sweeney <B00980368>
2025-11-17 15:49:40 +0000

    integrate superclass subclass

--- SmartConsole/SmartDevice.cs
+++ SmartConsole/SmartDevice.cs
@@ -8,5 +8,49 @@ namespace SmartHomeController
 {
     internal class SmartDevice
     {
+        private int deviceId;
+        private string deviceName;
+        private bool status;
+
+        // public properties
+        public int DeviceId
+        {
+            get { return deviceId; }
+            set { deviceId = value; }   
+        }
+        public string DeviceName
+        {
+            get { return deviceName; }
+            set { deviceName = value; }
+        }
+        public bool Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+        
+        public SmartDevice(int id, string name, bool status)
+        {
+            this.deviceId = id;
+            this.deviceName = name;
+            this.status = status;
+        }
+
+        public void TurnOn()
+        {
+            Status = true;
+            Console.WriteLine($"{deviceName} with device ID {DeviceId} is now on.");
+        }
+
+        public void TurnOff()
+        {
+            Status = false;
+            Console.WriteLine($"{deviceName} with device ID {DeviceId} is now off.");
+        }
+
+        public virtual void GetStatus()
+        {
+            Console.WriteLine($"Device ID: {DeviceId}, Device Name: {deviceName}, Status (on or off): {(Status ? "On" : "Off")}");
+        }
     }
 }
--- SmartConsole/SmartLight.cs
+++ SmartConsole/SmartLight.cs
@@ -8,5 +8,32 @@ namespace SmartHomeController
 {
     internal class SmartLight : SmartDevice
     {
+        private double brightness;
+        private string color;
+
+        // public properties
+        public double Brightness
+        {
+            get { return brightness; }
+            set { brightness = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        // constructor
+        public SmartLight(int deviceID, string deviceName, double brightness, string color) : base(deviceID, deviceName)
+        {
+            Brightness = brightness;
+            Color = color;
+        }
+
+        public override void GetStatus()
+        {
+            base.GetStatus();
+            Console.WriteLine($"Brightness: {Brightness}%, Color: {Color}");
+        }
+    }
     }
 }
